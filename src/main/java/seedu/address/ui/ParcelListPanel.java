de24e6e79dd1ea900794b53c9fca62d0266c59cd
visisapotato <vikviklim@msn.com>
2017-11-13 15:33:47 +0800

    list header

--- src/main/java/seedu/address/ui/ParcelListPanel.java
+++ src/main/java/seedu/address/ui/ParcelListPanel.java
@@ -2,6 +2,7 @@ package seedu.address.ui;
 
 import java.util.logging.Logger;
 
+import javafx.geometry.Orientation;
 import org.fxmisc.easybind.EasyBind;
 
 import com.google.common.eventbus.Subscribe;
@@ -11,6 +12,8 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.EventsCenter;
@@ -39,6 +42,12 @@ public class ParcelListPanel extends UiPart<Region> {
 
     @FXML
     private TabPane tabPanePlaceholder;
+
+    @FXML
+    private ScrollPane scrollPanePlaceholder1;
+
+    @FXML
+    private ScrollPane scrollPanePlaceholder2;
 
     public ParcelListPanel(ObservableList<ReadOnlyParcel> uncompletedParcels,
                            ObservableList<ReadOnlyParcel> completedParcels) {
